98af1326e05abde51aec2123c8b028f4c2f3b184
MJ_Shelton <mjshelton12@comcast.net>
2022-09-18 12:40:42 -0700

    Refractoring/bug fixes

--- public/js/login.js
+++ public/js/login.js
@@ -1,51 +1,46 @@
 const loginForm = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  const signupForm = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const psswrd = document.querySelector('#password-signup').value.trim();
-  
-    if (name && email && psswrd) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
+  }
+};
+
+const signupForm = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector("#signup-name").value.trim();
+  const email = document.querySelector("#signup-email").value.trim();
+  const psswrd = document.querySelector("#signup-psswrd").value.trim();
+
+  if (name && email && psswrd) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginForm);
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupForm);
-  
+  }
+};
+
+document.querySelector(".login-form").addEventListener("submit", loginForm);
+
+document.querySelector(".signup-form").addEventListener("submit", signupForm);
