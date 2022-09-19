a0f401930d0d90c2617d32b4a1b662c6a1cf2d48
MJ_Shelton <mjshelton12@comcast.net>
2022-09-19 13:39:02 -0700

    Starter build of comment feature

--- models/Comment.js
+++ models/Comment.js
@@ -15,9 +15,25 @@ Content.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
   }
 );
 
